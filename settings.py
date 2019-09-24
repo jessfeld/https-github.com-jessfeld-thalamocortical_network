1c47c211d0cf2b77a69ae31e5936d4cf6d680a5c
jchen6727 <jchen.6727@gmail.com>
2019-09-24 11:37:24 -0400

    added drug and mutant params

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Nav1.1 WT is 0, T875M is 1, W1204R is 2, R1648H is 3, R859C is 4, knock out is 5
-nav_type = 0
+nav_type = 2
 
 # no drug is 0, carbamazepine 1, oxcarbazepine 2, lamictal 3, eslicarb. 4, VPA 5, diazepam 6
 drug = 0
