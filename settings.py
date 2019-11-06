4b96b13b22589b36ccab840078686535a07f25e5
jchen6727 <jchen.6727@gmail.com>
2019-11-06 18:25:45 -0500

    deleted unused files

--- settings.py
+++
@@ -1,8 +0,0 @@
-# Nav1.1 WT is 0, T875M is 1, W1204R is 2, R1648H is 3, R859C is 4, knock out is 5
-nav_type = 2
-
-# no drug is 0, carbamazepine 1, oxcarbazepine 2, lamictal 3, eslicarb. 4, VPA 5, diazepam 6
-drug = 0
-
-#  percentage of full dose divided by 100. Full dose is 250nM (for Na channel drugs), which probably super-physiological
-dose = 0.05 
